7b5c8c2d72b0af773492a8ed2e7f2f2246395960
Long Luu <hailongluu@gmail.com>
2019-01-09 07:22:34 +0700

    first init

--- src/main/java/control/IRMIServer.java
+++ src/main/java/control/IRMIServer.java
@@ -7,8 +7,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface IRMIServer extends Remote {
-    ServerConfiguration getObjectServerDes(Student student, ServerConfiguration config) throws RemoteException;
-    ServerConfiguration getStringServerDes(Student student, ServerConfiguration config)throws RemoteException;
-    ServerConfiguration getNumericServerDes(Student student, ServerConfiguration config)throws RemoteException;
+    ServerConfiguration getObjectServerDes(Student student, ServerConfiguration config) ;
+    ServerConfiguration getStringServerDes(Student student, ServerConfiguration config);
+    ServerConfiguration getNumericServerDes(Student student, ServerConfiguration config);
 
 }
